e4c57004298374cfac1fc9e0cdb5701ee7aeb258
tuanteng96 <tuantengyn@gmail.com>
2024-01-12 18:05:55 +0700

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,13 @@
 import { ColorPicker } from "@wellbees/color-picker-input";
 import clsx from "clsx";
 import { useEffect, useRef, useState } from "react";
-import { exportComponentAsPNG } from "react-component-export-image";
 import { Controller, useForm } from "react-hook-form";
 import { NumericFormat } from "react-number-format";
 import html2canvas from "html2canvas";
+import axios from "axios";
 
 function Home() {
+  const [isLoading, setIsLoading] = useState(false);
   const { control, handleSubmit, watch } = useForm({
     defaultValues: {
       Title: {
@@ -59,29 +60,75 @@ function Home() {
     }
   }, [elRef, Width, Height]);
 
+  const getTokenParams = () => {
+    let url = window.location.href;
+    return url.split("token=")[1];
+  };
+
+  const onExportImage = async () => {
+    setIsLoading(true);
+    const canvas = await html2canvas(componentRef.current);
+    const image = canvas.toDataURL("image/png", 1.0);
+    var bodyFormData = new FormData();
+    bodyFormData.append("title", "mau-1-" + new Date().valueOf());
+    bodyFormData.append("base64", image);
+
+    axios
+      .post(`/api/v3/file?cmd=base64`, bodyFormData, {
+        headers: { Authorization: `Bearer ${getTokenParams()}` },
+      })
+      .then(({ data, error }) => {
+        if (!error) {
+          window?.parent?.postMessage(
+            JSON.stringify({
+              Image: data?.src || "",
+            }),
+            "*"
+          );
+        }
+        setIsLoading(false);
+      });
+  };
+
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-grow md:h-full flex-col md:flex-row"
+      className="flex flex-grow md:h-full flex-col md:flex-row h-full"
     >
       <div className="w-full md:w-[300px] md:min-w-[300px] lg:w-[400px] lg:min-w-[400px] p-5 bg-white h-full md:overflow-auto order-last md:order-first">
         <div className="md:hidden">
           <button
             type="button"
-            className="bg-primary text-white h-[38px] px-4 rounded flex items-center justify-center w-full mb-5"
-            onClick={async () => {
-              const canvas = await html2canvas(componentRef.current);
-              const image = canvas.toDataURL("image/png", 1.0);
-              window?.parent?.postMessage(
-                JSON.stringify({
-                  Image: image,
-                }),
-                "*"
-              );
-            }}
+            className="bg-primary text-white h-[38px] px-4 rounded flex items-center justify-center w-full mb-5 relative disabled:opacity-75"
+            onClick={onExportImage}
+            disabled={isLoading}
           >
             <ArrowDownTrayIcon className="w-5 mr-2" />
             Tải xuống
+            {isLoading && (
+              <div
+                role="status"
+                className="absolute right-3 top-2/4 -translate-y-2/4"
+              >
+                <svg
+                  aria-hidden="true"
+                  role="status"
+                  className="inline w-6 h-6 text-white animate-spin"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="#E5E7EB"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </div>
+            )}
           </button>
         </div>
         <div className="grid grid-cols-4 md:grid-cols-2 gap-5 mb-5">
@@ -512,16 +559,7 @@ function Home() {
             <button
               type="button"
               className="bg-primary text-white h-[42px] px-4 rounded flex items-center justify-center"
-              onClick={async () => {
-                const canvas = await html2canvas(componentRef.current);
-                const image = canvas.toDataURL("image/png", 1.0);
-                window?.parent?.postMessage(
-                  JSON.stringify({
-                    Image: image,
-                  }),
-                  "*"
-                );
-              }}
+              onClick={onExportImage}
             >
               <ArrowDownTrayIcon className="w-5 mr-2" />
               Tải xuống
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => ({
-    base: command === 'serve' ? '' : '/Thietke/source',
-    plugins: [react()]
-}))
+  base: command === "serve" ? "" : "/Thietke/source",
+  plugins: [react()],
+  server: {
+    port: 3001,
+  },
+}));
